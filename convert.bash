ba03c939a52aeb2340ce0fba0c065b58c3053d5f
John Lawrence Aspden <github@aspden.com>
2019-07-17 14:21:31 +0100

    script to convert 3gp files recorded by ankidroid to mp3 format, and also to modify the json decks to reflect this

--- convert.bash
+++ convert.bash
@@ -1,1 +1,14 @@
-mkdir convertmedia; for i in media/*.3gp ; do  j="${i/3gp/mp3}"; k="convertmedia${j/media}"; ffmpeg -i "$i" -c:a libmp3lame "$k" ; done
+#!/bin/bash
+for i in $(find . -name "*.3gp"); do
+    echo "$i" ;
+    j="${i/3gp/mp3}";
+    echo "$j"
+    echo ffmpeg -i "$i" -c:a libmp3lame "$j" 
+done
+
+for i in $(find . -name "*.json"); do
+    echo "$i" ;
+    echo sed -i 's/\.3gp/\.mp3' "$i"
+done
+
+
