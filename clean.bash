80d06e06003bf22b56e7fb96f10b952d4865a7b5
John Lawrence Aspden <github@aspden.com>
2019-07-18 18:39:05 +0100

    add new directories

--- clean.bash
+++ clean.bash
@@ -1,6 +1,7 @@
 cd ~/ankiexport
 rm -rf Latin__Latin_phrases/media
 rm -rf Latin__Lingua_Latina/media
-rm -rf Latin__Lingua_Latina/convertmedia
-rm -rf Latin__Latin_phrases/convertmedia
+rm -rf Latin__Wheelock_Latin_with_Audio/media
+rm -rf Latin__latin_misc/media
+
 
