8614e9c3a968e9677d6ee5f9ad2c19dc3e047ceb
John Lawrence Aspden <github@aspden.com>
2019-07-23 22:07:04 +0100

    add hashbang and a couple of find that seek out html

--- clean.bash
+++ clean.bash
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 cd ~/ankiexport
 rm -rf Latin__Latin_phrases/media
 rm -rf Latin__Lingua_Latina/media
@@ -5,4 +7,6 @@ rm -rf Latin__Wheelock_Latin_with_Audio/media
 rm -rf Latin__latin_misc/media
 rm -rf Latin__llspi_pensa/media
 
-
+# find pesky html that keeps getting sneaked in
+for i in $(find . -name "*.json"); do echo "$i" ; grep '<[/]*div[^>]*>' "$i" ; done
+for i in $(find . -name "*.json"); do echo "$i" ; grep '&nbsp;' "$i" ; done
