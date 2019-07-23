87bee6abaabe6d6cf0c6aa6b305a167d6a1a1880
John Lawrence Aspden <github@aspden.com>
2019-07-23 21:49:24 +0100

    add directory

--- clean.bash
+++ clean.bash
@@ -3,5 +3,6 @@ rm -rf Latin__Latin_phrases/media
 rm -rf Latin__Lingua_Latina/media
 rm -rf Latin__Wheelock_Latin_with_Audio/media
 rm -rf Latin__latin_misc/media
+rm -rf Latin__llspi_pensa/media
 
 
