32101e289fc49d4163404a4602a40cc338b182dd
John Lawrence Aspden <github@aspden.com>
2019-09-01 18:22:34 +0100

    add poems

--- clean.bash
+++ clean.bash
@@ -8,10 +8,11 @@ rm -rf Latin__Lingua_Latina/media
 rm -rf Latin__Wheelock_Latin_with_Audio/media
 rm -rf Latin__latin_misc/media
 rm -rf Latin__llspi_pensa/media
+rm -rf Latin__Latin_Poetry/media
 
 rm Latin__Latin_phrases/Latin*.json
 rm Latin__Lingua_Latina/Latin*.json
 rm Latin__Wheelock_Latin_with_Audio/Latin*.json
 rm Latin__latin_misc/Latin*.json
 rm Latin__llspi_pensa/Latin*.json
-
+rm Latin__Latin_Poetry/Latin*.json
