80e9f5d001fce30d7a9d33412ca2a8f29c933391
John Lawrence Aspden <github@aspden.com>
2019-09-24 15:02:13 +0100

    explain what this file is doing

--- convert.bash
+++ convert.bash
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# Convert all sound files from the 3gp recorded by ankidroid to mp3s, which are more universal
+
+# .3gp files, as recorded by ankidroid, won't play everywhere, so convert them to mp3
 for i in $(find . -name "*.3gp"); do
     echo "$i" ;
     j="${i/3gp/mp3}";
@@ -6,11 +10,14 @@ for i in $(find . -name "*.3gp"); do
     ffmpeg -i "$i" -c:a libmp3lame "$j" && rm "$i"
 done
 
+# then fiddle the json files so that the sound references point to the new files
 for i in $(find . -name "*.json"); do
     echo "$i" ;
     sed -i 's/\.3gp/\.mp3/g' "$i"
 done
 
+# If you ask ankidroid to re-record an mp3, it will, but in 3gp format
+# catch all the cases where this has happened and re-convert them
 for i in $(find . -name "*.mp3"); do
     if [[ $(file -b "$i") == "ISO Media, MPEG v4 system, 3GPP" ]] ; then
     echo "$i" ;
